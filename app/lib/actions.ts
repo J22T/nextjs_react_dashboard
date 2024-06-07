899cd50caec703656e7eb1a1f40e2d338c4eb027
J22T <66581302+J22T@users.noreply.github.com>
2024-06-06 23:23:11 -0700

    chatper 12 mutating data; forms; server actions; creating invoice; updating invoice; deleting invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,10 +13,48 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+
+
+
+
+export async function deleteInvoice(id: string) {
+    await sql `DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
+
+
+
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+     
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+
+
+
+
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const { customerId, amount, status } = CreateInvoice.parse ({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
