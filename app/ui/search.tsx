9b402ce7d0ba42326c5baf57a637cbfd4089cb07
J22T <66581302+J22T@users.noreply.github.com>
2024-06-07 00:27:52 -0700

    used const instead of function; tyring to resolve "useDebounceCallback"; course doesn't fully explain nor detail

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,13 +5,19 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const SearchParams = useSearchParams();
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  
-  function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
-    const params = new URLSearchParams(SearchParams);
+
+
+
+
+
+  // function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>)=>{
+      // console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    const term: string =e.target.value
     params.set('page', '1');
     if (term) {
       params.set('query', term);
@@ -20,7 +26,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace (`${pathname}?${params.toString()}`);
   }, 300);
-}
+  // console.log(term);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,10 +36,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={SearchParams.get('query')?.toString()}
+        // onChange={(e) => {
+        //   handleSearch(e.target.value);
+        // }}
+        onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
