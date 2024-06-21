c1be850dbcb84959d3e9b8acb34528355ed250fd
Jordan Taylor <66581302+J22T@users.noreply.github.com>
2024-06-21 13:27:02 -0700

    fixed homepage styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,18 +5,25 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata= {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Acme Dashboard | @Jordan Taylor',
+    default: 'Acme Dashboard by @Jordan Taylor',
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  authors: [{name: 'Jordan Taylor', url:'https://github.com/J22T/nextjs_dashboard'}],
+  keywords: ['Next.js 14', 'Acme', 'Dashboard', 'nextjs.org/learn', 'Server Actions'],
+  openGraph: {
+    title: 'Acme Dashboard',
+    description: 'The official Next.js Learn Dashboard buiilt with App Router.',
+    url: 'https://nextjs-dashboard-xi-six-94.vercel.app/',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
-      <body
-              className={`${inter.className} antialiased`}>{children}
+      <body className={`${inter.className} antialiased`}>{children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -13,41 +13,44 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div
-  className={styles.shape} />
-          <p
-              className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+          <div className={styles.shape} />
+          <div className={`${lusitana.className} text-base text-gray-800 md:text-3xl md:leading-normal`}>
+          {' '}
+            <p>
+              Welcome to Acme.
+            </p>
+          </div>
+          <div className="text-blue-500 text-xl">
+            <a href="https://nextjs.org/learn/">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </div>
+          <div className="text-lg text-gray-400">
+            <p>
+              from Vercel.
+            </p>
+          </div>
+          <Link href="/login" className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
+            Log in <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-          <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={760}
-          className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className="sm:block hidden"
-          alt="Screenshot of the dashboard project showing mobile version"
-          />
-        </div>
+          <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            {/* Add Hero Images Here */}
+            <Image
+              src="/hero-desktop.png"
+              width={1000}
+              height={760}
+              className="hidden md:block"
+              alt="Screenshots of the dashboard project showing desktop version"
+            />
+            <Image
+              src="/hero-mobile.png"
+              width={560}
+              height={620}
+              className="block md:hidden"
+              alt="Screenshot of the dashboard project showing mobile version"
+            />
+          </div>
       </div>
     </main>
   );
