9c12aaaa145f7b6c1b4a1a58f8a62ec8df757228
Jordan Taylor <66581302+J22T@users.noreply.github.com>
2024-06-21 22:19:29 -0700

    modified next.config.js file to mjs; added ppr functionality.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { Metadata } from 'next';
 
+export const experimental_ppr = true;
+
 export const metadata: Metadata = {
   title: {
     template: '%s | dashboard',
@@ -10,6 +12,10 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
  
+
+
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    experimental: {
+        ppr: 'incremental',
+    },
+};
+
+module.exports = nextConfig;
