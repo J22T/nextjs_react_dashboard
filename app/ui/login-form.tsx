697bb705deaecdd73f0ae04f289bf48c8e0d9ac1
Jordan Taylor <66581302+J22T@users.noreply.github.com>
2024-06-22 10:47:09 -0700

    update

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,23 +8,26 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-// import { useFormState, useFormStatus } from 'react-dom';
-import { useActionState } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
+// import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
+  const [errorMessage, dispatch] = useFormState(
     authenticate,
-    undefined,
+    undefined
   );
 
 // export default function LoginForm() {
-//   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+//   const [errorMessage, formAction, isPending, dispatch] = useActionState(
+//     authenticate,
+//     undefined,
+//   );
 
 // Previously, was using {dispatch}. Don't know / recall why dispatch was there.
   return (
-    // <form action={dispatch} className="space-y-3">
-    <form action={formAction} className="space-y-3">
+    // <form action={formAction} className="space-y-3">
+    <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -70,10 +73,10 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        {/* <LoginButton /> */}
-        <button className="mt-4 w-full" aria-disabled={isPending}>
+        <LoginButton />
+        {/* <button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </button>
+        </button> */}
         <div
             className="flex h-8 items-end space-x-1"
             aria-live="polite"
@@ -92,12 +95,12 @@ export default function LoginForm() {
   );
 }
 
-// function LoginButton() {
-//   const { pending } = useFormStatus();
+function LoginButton() {
+  const { pending } = useFormStatus();
 
-//   return (
-//     <Button className="mt-4 w-full" aria-disabled={pending}>
-//         Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-//     </Button>
-//   );
-// }
+  return (
+    <Button className="mt-4 w-full" aria-disabled={pending}>
+        Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
+  );
+}
--- auth.ts
+++ auth.ts
@@ -42,7 +42,7 @@ export const { auth, signIn, signOut } = NextAuth({
                     if (passwordsMatch) return user;
                 }
                 
-                // console.log('Invalid credentials');
+                console.log('Invalid credentials');
                 return null;
             },
         }),
